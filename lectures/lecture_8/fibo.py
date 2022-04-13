acda0cc6caa0c1ac5b7ac4ef32c257f29a19581f
Гудков Вадим Вадимович <gudkov.v@medlinx.online>
2022-04-13 23:26:39 +0300

    more lectures

--- lectures/lecture_8/fibo.py
+++ lectures/lecture_8/fibo.py
@@ -1,5 +1,13 @@
-from os import listdir
+import os
+import sys
 
+
+a = 2
+
+
+class Hello:
+    """I am class which greets"""
+    pass
 
 def fib(n):
     a, b = 0, 1
@@ -15,3 +23,7 @@ def fib2(n):
         result.append(a)
         a, b = b, a+b
     return result
+
+print(dir())
+if __name__ == "__main__":
+    fib2(int(sys.argv[1]))

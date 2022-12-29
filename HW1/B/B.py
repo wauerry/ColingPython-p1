a574a0a4dd0e8d2c22248a224bc3fcb262fb1e07
wauerry <117657671+wauerry@users.noreply.github.com>
2022-12-29 03:40:55 +0300

    Added B task solution

--- HW1/B/B.py
+++ HW1/B/B.py
@@ -1,3 +1,11 @@
+penguin1 = '   _~_   '
+penguin2 = '  (o o)  '
+penguin3 = ' /  V  \\ '
+penguin4 = '/(  _  )\\'
+penguin5 = '  ^^ ^^  '
+
+
 def solution(n):
-    ### YOUR SOLUTION ###
-    return
+    if n == 0:
+        return ''
+    return penguin1 * n + '\n' + penguin2 * n + '\n' + penguin3 * n + '\n' + penguin4 * n + '\n' + penguin5 * n
